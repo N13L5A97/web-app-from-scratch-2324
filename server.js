0ed3d6cc87694955c9854cb4e41046ddfafeb656
Niels Aling <niels.aling@hva.nl>
2024-02-08 13:10:53 +0100

    Update user data and social links

--- server.js
+++ server.js
@@ -15,15 +15,16 @@ app.use(express.static("public"));
 
 // home
 app.get("/", async function (req, res) {
-  try {
-    const playlists = await getMyPlaylists();
-    // const userData = await fetchUserData();
+    const userData = await fetchUserData();
+    const userImage = userData.images[0].black;
+    const userSocials = userData.socials;
 
-    res.render("pages/index", { playlists });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
-  }
+    console.log("User data: ")
+    console.log(userSocials[0].github);
+
+    const playlists = await getMyPlaylists();
+
+    res.render("pages/index", { playlists, userData, userImage, userSocials });
 });
 
 // login
@@ -58,7 +59,7 @@ app.get("/callback", async function (req, res) {
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`listening on port ${port}`);
 });
 
 // not used
@@ -74,8 +75,7 @@ app.get("/playlists", async function (req, res) {
 });
 
 
-// functions
-
+// FUNCTIONS
 
 // spotify token
 var authOptions = {
@@ -118,75 +118,72 @@ const getMyPlaylists = async () => {
   return (playlistItems);
 };
 
+//get id's of all the playlists (NOT USED)
 
-//get id's of all the playlists
-const getPlaylistsIds = async () => {
-  const response = await fetch(authOptions.url, {
-    method: "POST",
-    body: querystring.stringify(authOptions.form),
-    headers: authOptions.headers,
-  });
+// const getPlaylistsIds = async () => {
+//   const response = await fetch(authOptions.url, {
+//     method: "POST",
+//     body: querystring.stringify(authOptions.form),
+//     headers: authOptions.headers,
+//   });
 
-  const token = await response.json();
+//   const token = await response.json();
 
-  const playlists = await fetch(
-    "https://api.spotify.com/v1/users/niels.aling/playlists",
-    {
-      headers: {
-        Authorization: "Bearer " + token.access_token,
-      },
-    }
-  );
+//   const playlists = await fetch(
+//     "https://api.spotify.com/v1/users/niels.aling/playlists",
+//     {
+//       headers: {
+//         Authorization: "Bearer " + token.access_token,
+//       },
+//     }
+//   );
 
-  const playlistsJson = await playlists.json();
-  const playlistItems = playlistsJson.items;
-  const playlistIds = playlistItems.map((playlist) => playlist.id);
+//   const playlistsJson = await playlists.json();
+//   const playlistItems = playlistsJson.items;
+//   const playlistIds = playlistItems.map((playlist) => playlist.id);
 
-  return (playlistIds);
-};
+//   return (playlistIds);
+// };
 
-//get info of all the playlists
-const getPlaylistInfo = async () => {
-  const response = await fetch(authOptions.url, {
-    method: "POST",
-    body: querystring.stringify(authOptions.form),
-    headers: authOptions.headers,
-  });
+//get info of all the playlists (NOT USED)
+// const getPlaylistInfo = async () => {
+//   const response = await fetch(authOptions.url, {
+//     method: "POST",
+//     body: querystring.stringify(authOptions.form),
+//     headers: authOptions.headers,
+//   });
 
-  const token = await response.json();
+//   const token = await response.json();
 
-  const playlistIds = await getPlaylistsIds();
+//   const playlistIds = await getPlaylistsIds();
 
-  const playlists = [];
+//   const playlists = [];
 
-  for (let i = 0; i < playlistIds.length; i++) {
-    const playlist = await fetch(
-      `https://api.spotify.com/v1/playlists/${playlistIds[i]}`,
-      {
-        headers: {
-          Authorization: "Bearer " + token.access_token,
-        },
-      }
-    ).then(response => response.json());
-    playlists.push(playlist);
+//   for (let i = 0; i < playlistIds.length; i++) {
+//     const playlist = await fetch(
+//       `https://api.spotify.com/v1/playlists/${playlistIds[i]}`,
+//       {
+//         headers: {
+//           Authorization: "Bearer " + token.access_token,
+//         },
+//       }
+//     ).then(response => response.json());
+//     playlists.push(playlist);
     
-    }
+//     }
 
-    // console.log(playlists);
-    return playlists;
-};
+//     // console.log(playlists);
+//     return playlists;
+// };
 
 //fetch data from json file
-// const fetchUserData = async () => {
-//   fs.readFile("data.json", "utf8", (err, data) => {
-//     const userData = JSON.parse(data);
-//     console.log(userData.name);
-//     if (err) {
-//       console.error(err);
-//       return;
-//     }});
 
-//     return userData;
-// }
-
-// fetchUserData();
+const fetchUserData = async () => {
+  try{
+    const data = fs.readFileSync("data.json", "utf8");
+    userData = JSON.parse(data);
+    return userData;
+  } catch (error) {
+    console.error(error);
+  }
+};
